d11903a0202d7e639b8a567a6d91f3170c715df9
manirajnadar <manirajnadar07@gmail.com>
2024-01-04 18:13:50 +0530

    Cors chagnes  aded

--- config/cors.php
+++ config/cors.php
@@ -19,10 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'http://localhost:3000', // Allow requests from the local development environment
-        'https://root.development.glintix.com/', // Allow requests from your React.js app's production domain
-    ],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
